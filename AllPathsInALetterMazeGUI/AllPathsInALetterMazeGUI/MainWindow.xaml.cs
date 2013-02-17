464a9899bd8230c6178361ad70ff22f81aba6929
Калоян <kalala@mail.bg>
2013-02-17 21:01:50 +0200

    program for finding all paths in a maze with GUI

---
+++ AllPathsInALetterMazeGUI/AllPathsInALetterMazeGUI/MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AllPathsInALetterMazeGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Maze maze;
+        private Grid MazeGrid;
+        private StackPanel PathsPanel;
+        private const string FileInputBoxIdleText = "Browse for file...";
+        private const string RandomMazeFilePath = "random.in";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadMazeFromFile(string filePath)
+        {
+            if (maze == null)
+            {
+                GeneralGrid.Children.Clear();
+                GeneralGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+                MazeGrid = new Grid();
+                MazeGrid.ShowGridLines = true;
+
+                GeneralGrid.Children.Add(MazeGrid);
+                Grid.SetColumn(MazeGrid, 0);
+
+                maze = new Maze(filePath);
+                for (int row = 0; row < maze.maze.GetLength(0); row++)
+                {
+                    MazeGrid.RowDefinitions.Add(new RowDefinition());
+                }
+                for (int col = 0; col < maze.maze.GetLength(1); col++)
+                {
+                    MazeGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                }
+
+                for (int row = 0; row < maze.maze.GetLength(0); row++)
+                {
+                    for (int col = 0; col < maze.maze.GetLength(1); col++)
+                    {
+                        TextBlock letter = new TextBlock();
+                        letter.Text = maze.maze[row, col].Value.ToString();
+                        letter.HorizontalAlignment = HorizontalAlignment.Center;
+                        letter.VerticalAlignment = VerticalAlignment.Center;
+                        letter.FontSize = 25;
+                        MazeGrid.Children.Add(letter);
+                        Grid.SetRow(letter, row);
+                        Grid.SetColumn(letter, col);
+                    }
+                }
+                Button findPathsButton = new Button();
+                findPathsButton.Content = "Find minimal paths to exits";
+                findPathsButton.Height = 30;
+                findPathsButton.Click += new RoutedEventHandler(FindPathsButton_Click);
+                GeneralGrid.Children.Add(findPathsButton);
+                Grid.SetColumn(findPathsButton, 1);
+            }
+        }
+
+        private void FindPathsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (maze != null)
+            {
+                GeneralGrid.Children.Remove((Button)sender);
+
+                
+
+                ScrollViewer scroll = new ScrollViewer();
+                scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+                scroll.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+                GeneralGrid.Children.Add(scroll);
+                Grid.SetColumn(scroll, 1);
+                
+
+                PathsPanel = new StackPanel();
+                scroll.Content = PathsPanel;
+
+                TextBlock description = new TextBlock();
+                description.Text = "All paths to exits:";
+                description.Foreground = Brushes.Gray;
+                description.FontSize = 10;
+                description.Padding = new Thickness(20d, 0d, 0d, 0d);
+                PathsPanel.Children.Add(description);
+
+                List<Path> paths = maze.FindAllPaths();
+                description.Text += " " + paths.Count.ToString();
+                for (int i = 0; i < paths.Count; i++)
+                {
+                    Path path = paths[i];
+                    TextBlock pathToText = new TextBlock();
+                    pathToText.Text = path.ToString();
+                    pathToText.Padding = new Thickness(20d, 0, 0, 0);
+                    pathToText.FontSize = 30;
+                    pathToText.MouseEnter += new MouseEventHandler((text, events) => pathToText_MouseEnter(text, events, path));
+                    pathToText.MouseLeave += new MouseEventHandler((text, events) => pathToText_MouseLeave(text, events, path));
+                    PathsPanel.Children.Add(pathToText);
+                }
+            }
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog =
+                new Microsoft.Win32.OpenFileDialog();
+            dialog.DefaultExt = ".in";
+            dialog.Filter = "Specific maze files |*.in";
+
+            Nullable<bool> result = dialog.ShowDialog();
+
+            // Get the selected file and send it to a textbox
+            string filePath = FilePathBox.Text;
+            if (result == true)
+            {
+                filePath = dialog.FileName;
+                FilePathBox.Text = filePath;
+            }
+            ReadMazeFromFile(filePath);
+        }
+
+        private void RandomButton_Click(object sender, RoutedEventArgs e)
+        {
+            Random rand = new Random();
+            char[] letters = new char[26];
+            for (int i = 'a'; i <= 'z'; i++)
+            {
+                letters[i-(int)'a'] = (char)i;
+            }
+            int size = rand.Next(2, 10); // note that in a 10 x 10 maze there can thousands of exits with the current letter load facotr of 65%, if I reduce the letters there won't be so many exits or reduce the size if it's slow
+            StreamWriter writer = new StreamWriter(RandomMazeFilePath);
+            using (writer)
+            {
+                List<char> charsToInsert = new List<char>();
+                for (int i = 0; i < size * size - 1; i++)
+                {
+                    bool addLetter = rand.Next() % 2 == 0 || (rand.Next() % 2 == 0 && rand.Next() % 2 == 0); // that's actually kinda clever - gives us about 65% chance of having a letter, which is a good balance
+                    if (addLetter)
+                    {
+                        charsToInsert.Add(letters[rand.Next(0, letters.Length)]);
+                    }
+                    else
+                    {
+                        charsToInsert.Add('#');
+                    }
+                }
+                charsToInsert.Insert(rand.Next(0, charsToInsert.Count), '*');
+                writer.WriteLine(size);
+                for (int row = 0; row < size; row++)
+                {
+                    for (int col = 0; col < size; col++)
+                    {
+                        char randomChar = charsToInsert[0];
+                        charsToInsert.Remove(randomChar);
+                        writer.Write(randomChar);
+                        if (col < size - 1)
+                        {
+                            writer.Write(" ");
+                        }
+                    }
+                    writer.WriteLine();
+                }
+            }
+            ReadMazeFromFile(RandomMazeFilePath);
+        }
+
+        private void pathToText_MouseEnter(object sender, MouseEventArgs e, Path path)
+        {
+            TextBlock _sender = (TextBlock)sender;
+            _sender.FontSize = 40;
+            _sender.Foreground = Brushes.Red;
+            foreach (Cell cell in path.Cells)
+            {
+                TextBlock letter = MazeGrid.Children.Cast<TextBlock>()
+                    .First(el => Grid.GetRow(el) == cell.Row && Grid.GetColumn(el) == cell.Column);
+                letter.Foreground = Brushes.Red;
+            }
+        }
+
+
+        private void pathToText_MouseLeave(object sender, MouseEventArgs e, Path path)
+        {
+            TextBlock _sender = (TextBlock)sender;
+            _sender.FontSize = 30;
+            _sender.Foreground = Brushes.Black;
+            foreach (Cell cell in path.Cells)
+            {
+                TextBlock letter = MazeGrid.Children.Cast<TextBlock>()
+                    .First(el => Grid.GetRow(el) == cell.Row && Grid.GetColumn(el) == cell.Column);
+                letter.Foreground = Brushes.Black;
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ AllPathsInALetterMazeGUI/AllPathsInALetterMazeGUI/Maze.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AllPathsInALetterMazeGUI
+{
+    public class Maze
+    {
+        internal Cell[,] maze;
+        private Cell startingPos;
+        private List<Path> paths;
+
+        public Maze(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(String.Format("File \"{0}\" was not found.", filePath));
+            }
+            StreamReader reader = new StreamReader(filePath);
+            startingPos = null;
+            int size = int.Parse(reader.ReadLine()); // it's always in the first line by definition
+            this.maze = new Cell[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                string line = reader.ReadLine();
+                for (int p = 0; p < line.Length; p += 2)
+                {
+                    maze[i, p / 2] = new Cell(i, p / 2, line[p], null);
+                    if (line[p] == '*')
+                    {
+                        startingPos = maze[i, p / 2];
+                    }
+                }
+            }
+            if (startingPos == null)
+            {
+                throw new Exception("No staring posiiton in the maze.");
+            }
+            reader.Close();
+        }
+
+        public List<Path> FindAllPaths()
+        {
+            paths = new List<Path>();
+            Queue<Cell> queue = new Queue<Cell>();
+            queue.Enqueue(startingPos);
+            while (queue.Count > 0)
+            {
+                Cell current = queue.Dequeue();
+
+                Cell leftNeighbour = GetLeftNeighbour(current);
+                if (leftNeighbour == null)
+                {
+                    Path newPath = new Path(current);
+                    if (!paths.Contains(newPath))
+                        paths.Add(newPath);
+                }
+                else if (leftNeighbour.Value != '#')
+                {
+                    Cell leftNeighbourUpdated = new Cell(leftNeighbour.Row, leftNeighbour.Column,
+                        leftNeighbour.Value, current);
+                    if (!current.HasPredecessor(leftNeighbourUpdated)) // if not already has predecessor
+                    {
+                        queue.Enqueue(leftNeighbourUpdated);
+                    }
+                }
+
+                Cell topNeighbour = GetTopNeighbour(current);
+                if (topNeighbour == null)
+                {
+                    Path newPath = new Path(current);
+                    if (!paths.Contains(newPath))
+                        paths.Add(newPath);
+                }
+                else if (topNeighbour.Value != '#')
+                {
+                    Cell topNeighbourUpdated = new Cell(topNeighbour.Row, topNeighbour.Column,
+                        topNeighbour.Value, current);
+                    if (!current.HasPredecessor(topNeighbourUpdated))
+                    {
+                        queue.Enqueue(topNeighbourUpdated);
+                    }
+                }
+
+                Cell rightNeighbour = GetRightNeighbour(current);
+                if (rightNeighbour == null)
+                {
+                    Path newPath = new Path(current);
+                    if (!paths.Contains(newPath))
+                        paths.Add(newPath);
+                }
+                else if (rightNeighbour.Value != '#')
+                {
+                    Cell rightNeighbourUpdated = new Cell(rightNeighbour.Row, rightNeighbour.Column,
+                        rightNeighbour.Value, current);
+                    if (!current.HasPredecessor(rightNeighbourUpdated))
+                    {
+                        queue.Enqueue(rightNeighbourUpdated);
+                    }
+                }
+
+                Cell bottomNeighbour = GetBottomNeighbour(current);
+                if (bottomNeighbour == null)
+                {
+                    Path newPath = new Path(current);
+                    if (!paths.Contains(newPath))
+                        paths.Add(newPath);
+                }
+                else if (bottomNeighbour.Value != '#')
+                {
+                    Cell bottomNeighbourUpdated = new Cell(bottomNeighbour.Row, bottomNeighbour.Column,
+                        bottomNeighbour.Value, current);
+                    if (!current.HasPredecessor(bottomNeighbourUpdated)) 
+                    {
+                        queue.Enqueue(bottomNeighbourUpdated);
+                    }
+                }
+            }
+            return paths;
+        }
+
+
+        public Cell GetLeftNeighbour(Cell current)
+        {
+            if (current.Column - 1 < 0)
+            {
+                return null; // indicates reaching an exit
+            }
+            return maze[current.Row, current.Column - 1];
+        }
+
+        public Cell GetTopNeighbour(Cell current)
+        {
+            if (current.Row - 1 < 0)
+            {
+                return null;
+            }
+            return maze[current.Row - 1, current.Column];
+        }
+
+        public Cell GetRightNeighbour(Cell current)
+        {
+            if (current.Column + 1 >= maze.GetLength(1))
+            {
+                return null;
+            }
+            return maze[current.Row, current.Column + 1];
+        }
+
+        public Cell GetBottomNeighbour(Cell current)
+        {
+            if (current.Row + 1 == maze.GetLength(0))
+            {
+                return null;
+            }
+            return maze[current.Row + 1, current.Column];
+        }
+
+    }
+
+    public class Cell
+    {
+        private int row;
+        private int col;
+        private char value;
+        private Cell predecessor;
+
+        public Cell(int row, int col, char value, Cell predecessor)
+        {
+            this.row = row;
+            this.col = col;
+            this.value = value;
+            this.predecessor = predecessor;
+        }
+
+        public int Row
+        {
+            get
+            {
+                return this.row;
+            }
+            set
+            {
+                this.row = value;
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                return this.col;
+            }
+            set
+            {
+                this.col = value;
+            }
+        }
+
+        public char Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public Cell Predecessor
+        {
+            get
+            {
+                return this.predecessor;
+            }
+            set
+            {
+                this.predecessor = value;
+            }
+        }
+
+        public bool HasPredecessor(Cell other)
+        {
+            Path pathSoFar = new Path(this);
+            if (pathSoFar.ContainsCell(other))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.value.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            Cell other = (Cell)obj;
+            if (this.row == other.Row && this.Column == other.Column)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return row.GetHashCode() * col.GetHashCode() * value.GetHashCode();
+        }
+
+
+
+    }
+
+    public class Path
+    {
+        private List<Cell> cells;
+
+        public Path(Cell exitCell)
+        {
+            cells = new List<Cell>();
+            Cell current = exitCell;
+            while (current != null)
+            {
+                cells.Add(current);
+                current = current.Predecessor;
+            }
+            cells.Reverse();
+        }
+        public Path(List<Cell> cells)
+        {
+            this.cells = cells;
+        }
+
+
+        public List<Cell> Cells
+        {
+            get
+            {
+                return this.cells;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            for (int i = 0; i < cells.Count; i++)
+            {
+                sb.Append(cells[i].Value + ", ");
+            }
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append("]");
+            return sb.ToString();
+        }
+
+        public bool ContainsCell(Cell cell)
+        {
+            if (cells.Contains(cell))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            Path other = (Path)obj;
+            if (this.cells.Count != other.cells.Count)
+                return false;
+            for (int i = 0; i < cells.Count; i++)
+            {
+                if (cells[i] != other.cells[i])
+                    return false;
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return cells.GetHashCode();
+        }
+    }
+            
+
+        
+}
